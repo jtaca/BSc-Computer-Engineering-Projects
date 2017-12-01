cf9c602d4813008d75932a4269dfdcfc6f98aab5
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2017-12-01 20:07:40 +0000

    Classes Partilhadas

---
+++ 2nd_Year/PCD/Projeto v13/Project - Googa-lhe - ClassesPartilhadas V13/src/classes_partilhadas/Connections.java
@@ -0,0 +1,7 @@
+package classes_partilhadas;
+
+public class Connections {
+	
+	public static final int PORTO = 8080;
+
+}
---
+++ 2nd_Year/PCD/Projeto v13/Project - Googa-lhe - ClassesPartilhadas V13/src/classes_partilhadas/TipoDeMensagem.java
@@ -0,0 +1,5 @@
+package classes_partilhadas;
+
+public enum TipoDeMensagem {
+	ENTER_CLIENT, SEARCH_WORD, GET_NEWS, ENTER_WORKER, GET_TASK, GIVE_RESULT, EXIT;
+}
